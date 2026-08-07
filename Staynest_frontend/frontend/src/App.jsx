c700ada7ece2d91e2e7e363c944311d4e6d0bbec
Yukti Payak <21301064yukti@viva-technology.org>
2026-08-07 22:16:40 +0530

    implemneted footer

--- Staynest_frontend/frontend/src/App.jsx
+++ Staynest_frontend/frontend/src/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ShowListing from "./pages/ShowListing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/listings/:id" element={<ShowListing />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      {/* Main content takes up available vertical space */}
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listings/:id" element={<ShowListing />} />
+        </Routes>
+      </div>
+
+      {/* Footer rendered at the bottom of all pages */}
+      <Footer />
+    </div>
   );
 }
 
